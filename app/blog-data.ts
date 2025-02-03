9af283faddcfd3d9db623d76f89cebc5da4408bd
Ayush <119443185+CrackedResearcher@users.noreply.github.com>
2025-02-03 22:17:59 +0530

    Update blog-data.ts for second-blog

--- app/blog-data.ts
+++ app/blog-data.ts
@@ -1,5 +1,11 @@
 export const BLOG_DATA = [
     {
+  "title": "second blog",
+  "description": "testing whether third blog setup works or not lets seee final testing",
+  "link": "/blog/second-blog",
+  "uid": "blog-1738601278602"
+},
+    {
   "title": "test blog 2",
   "description": "this is the new test blog second. second api integration testing",
   "link": "/blog/test-blog-2",
