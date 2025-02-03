a58d3fea4f3823d245604b593554a550ee247a60
Ayush <119443185+CrackedResearcher@users.noreply.github.com>
2025-02-04 00:26:57 +0530

    Update blog-data.ts

--- app/blog-data.ts
+++ app/blog-data.ts
@@ -1,39 +1,9 @@
 export const BLOG_DATA = [
     {
-  "title": "error fixing",
-  "description": "lets see whast the issue",
-  "link": "/blog/error-fixing",
-  "uid": "blog-1738608622517"
-},
-    {
-  "title": "just build things",
-  "description": "this is why building stuff matters more than anything else in this world",
-  "link": "/blog/just-build-things",
-  "uid": "blog-1738608239150"
-},
-    {
   "title": "just build cool stuff",
   "description": "that is how u learn to think and build things that eventually go onto change the world.",
   "link": "/blog/just-build-cool-stuff",
   "uid": "blog-1738608058786"
-},
-    {
-  "title": "publish blog one",
-  "description": "this is a new blog post getting run right now..",
-  "link": "/blog/publish-blog-one",
-  "uid": "blog-1738602399085"
-},
-    {
-  "title": "second blog",
-  "description": "testing whether third blog setup works or not lets seee final testing",
-  "link": "/blog/second-blog",
-  "uid": "blog-1738601278602"
-},
-    {
-  "title": "test blog 2",
-  "description": "this is the new test blog second. second api integration testing",
-  "link": "/blog/test-blog-2",
-  "uid": "blog-1738600570326"
 },
     {
       title: 'This is the first blog post, hello 👋',
