32dc1c92518476004df76fb60a9bc4882fa7d306
Ayush <119443185+CrackedResearcher@users.noreply.github.com>
2025-02-04 00:29:43 +0530

    Update blog-data.ts for test-blog-one

--- app/blog-data.ts
+++ app/blog-data.ts
@@ -1,5 +1,11 @@
 export const BLOG_DATA = [
     {
+  "title": "test blog one",
+  "description": "testign whether errors have gone or not",
+  "link": "/blog/test-blog-one",
+  "uid": "blog-1738609183698"
+},
+    {
   "title": "just build cool stuff",
   "description": "that is how u learn to think and build things that eventually go onto change the world.",
   "link": "/blog/just-build-cool-stuff",
