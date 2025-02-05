feb894259389fb9fc3a851b32d633dc0ab46f998
Ayush <119443185+CrackedResearcher@users.noreply.github.com>
2025-02-06 00:44:13 +0530

    Remove blog post with UID: blog-1738780630262

--- app/blog-data.ts
+++ app/blog-data.ts
@@ -1,32 +1,26 @@
 export const BLOG_DATA = [
-    {
-  "title": "my new bloggy",
-  "description": "hi there i'm testing out this 'so called' new bloggin tool.",
-  "link": "/blog/my-new-bloggy",
-  "uid": "blog-1738780630262"
-},
-    {
-  "title": "one two theree",
-  "description": "lets goooo!",
-  "link": "/blog/one-two-theree",
-  "uid": "blog-1738777154641"
-},
-    {
-  "title": "this is a new app",
-  "description": "hello let's debug",
-  "link": "/blog/this-is-a-new-app",
-  "uid": "blog-1738735959708"
-},
-    {
-  "title": "just build cool stuff",
-  "description": "that is how u learn to think and build things that eventually go onto change the world.",
-  "link": "/blog/just-build-cool-stuff",
-  "uid": "blog-1738608058786"
-},
-    {
-      "title": "This is the first blog post, hello 👋",
-      "description": "It doesnt need a desc!",
-      "link": "/blog/first-blog",
-      "uid": "blog-1",
-    },
-]
+  {
+    "title": "one two theree",
+    "description": "lets goooo!",
+    "link": "/blog/one-two-theree",
+    "uid": "blog-1738777154641"
+  },
+  {
+    "title": "this is a new app",
+    "description": "hello let's debug",
+    "link": "/blog/this-is-a-new-app",
+    "uid": "blog-1738735959708"
+  },
+  {
+    "title": "just build cool stuff",
+    "description": "that is how u learn to think and build things that eventually go onto change the world.",
+    "link": "/blog/just-build-cool-stuff",
+    "uid": "blog-1738608058786"
+  },
+  {
+    "title": "This is the first blog post, hello 👋",
+    "description": "It doesnt need a desc!",
+    "link": "/blog/first-blog",
+    "uid": "blog-1"
+  }
+];
