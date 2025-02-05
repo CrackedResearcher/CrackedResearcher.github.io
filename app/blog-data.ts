7ea076302433a619624c8970d8d5e1a24bc1c88d
Ayush <119443185+CrackedResearcher@users.noreply.github.com>
2025-02-05 11:42:39 +0530

    Update blog-data.ts for this-is-a-new-app

--- app/blog-data.ts
+++ app/blog-data.ts
@@ -1,5 +1,11 @@
 export const BLOG_DATA = [
     {
+  "title": "this is a new app",
+  "description": "hello let's debug",
+  "link": "/blog/this-is-a-new-app",
+  "uid": "blog-1738735959708"
+},
+    {
   "title": "just build cool stuff",
   "description": "that is how u learn to think and build things that eventually go onto change the world.",
   "link": "/blog/just-build-cool-stuff",
